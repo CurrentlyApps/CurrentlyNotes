9e2278d597441723c73f23ca84d7972e63e42f8f
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-10-20 10:05:42 +0800

    Firebase config uses env file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,9 +3,16 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/
 import { set, getDatabase, ref } from "firebase/database";
 
 const firebaseConfig = {
-    
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
-  
+
 export const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
