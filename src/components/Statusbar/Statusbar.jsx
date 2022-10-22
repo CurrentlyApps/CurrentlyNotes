17964f8e8ed3d161603891aee2e9574bf7e11897
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-10-22 15:14:17 +0800

    Modified top right dropdown to use flowbite component

--- src/components/Statusbar/Statusbar.jsx
+++ src/components/Statusbar/Statusbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { AppContext } from "../Home"
 import { signIn, signOutClick } from "../../services/firebase"
+import { Dropdown } from "flowbite-react"
 
 export default function Statusbar() {
     const context = useContext(AppContext)
@@ -12,7 +13,7 @@ export default function Statusbar() {
     return (
         <div className="w-full h-30 text-white bg-black px-4 py-2 flex justify-between">
             <div className="lg:font-extrabold w-1/3 text-lg mr-auto flex flex-row cursor-pointer" onClick={ () => { context.setExtendedState(!context.extended) } }>
-                <div className="py-auto align-middle py-auto my-auto mr-5 tracking-widest uppercase">Current</div>
+                <div className="py-auto align-middle py-auto my-auto mr-5 tracking-widest uppercase">Currently</div>
              
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`${context.extended ? 'rotate-180' : ''} transition-all w-5 h-5 my-auto ml-2`}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
@@ -28,31 +29,46 @@ export default function Statusbar() {
                     <div className="text-md flex flex-row gap-4">
                         <div
                         onClick={ () => { setState(false, true) } }
-                        className={`${context.editState ? "text-zinc-500 cursor-pointer" : "text-white"}`}>
+                        className={`transition-all ${context.editState ? "text-zinc-500 cursor-pointer" : "text-white underline underline-offset-8 decoration-2"}`}>
                             View
                         </div>
                         <div
                         onClick={ () => { setState(true, false) } }
-                        className={`${context.editState ? "text-white" : "text-zinc-500 cursor-pointer"}` }>
+                        className={`transition-all ${context.editState ? "text-white underline underline-offset-8 decoration-2" : "text-zinc-500 cursor-pointer"}` }>
                             Edit
                         </div>
                     </div>
                 }
             </div>
 
-            <div className="w-1/3 mr-3 flex justify-end ">
-                <div className="lg:flex hidden">
-                { context.user == null
-                 ? <div className="cursor-pointer text-sm text-white py-2" onClick={ () => {signIn(context)} }> Login</div>
-                 : <div className="dropDown">
-                    <div className="hidden lg:block py-2">{ context.user.displayName }</div>
-                    <div className="block md:hidden py-2"> o </div>
-                    <div className="dropDownContent w-full">
-                        <div className="cursor-pointer text-sm text-black" onClick={ () => {signOutClick(context)} }>Logout</div>
-                    </div>
-                   </div>
-                }
-                </div>
+            <div className="lg:flex hidden w-1/3 mr-3 justify-end ">
+                <Dropdown label={ context.user ? context.user.displayName : 'Not Logged In'}>
+                    <Dropdown.Header>
+                        { 
+                            context.user == null
+                            ? <div className="cursor-pointer text-md py-2" onClick={ () => {signIn(context)} }> Login</div>
+                            : <div className="dropDown">
+                                <div className="flex flex-row">
+                                    <div className="hidden lg:block">{ context.user.displayName }</div>
+                                </div>
+                                <span className="block text-sm font-medium truncate">
+                                    { context.user.email }
+                                </span>
+                            </div>
+                        }
+                        
+                    </Dropdown.Header>
+                    <Dropdown.Item>
+                        <a href="https://www.currentlynotes.com/">Homepage</a>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                        Settings
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item  onClick={ () => {signOutClick(context)} }>
+                        Sign out
+                    </Dropdown.Item>
+                </Dropdown>
             </div>
         </div>
     )
