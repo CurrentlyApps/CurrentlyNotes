2404e7b1d08de3e64af2b9517ebace5174c41012
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-10-24 17:57:48 +0800

    Truncated long title names in side bar and made text small

--- src/components/SidePanel/NoteListEntry.jsx
+++ src/components/SidePanel/NoteListEntry.jsx
@@ -9,10 +9,10 @@ export default function NoteListEntry(props) {
     const context = useContext(AppContext)
     const note = props.note
     return (
-        <div key={note.id} className="flex flex-row transition-all hover:bg-zinc-300">
+        <div key={note.id} className={`flex flex-row transition-all hover:bg-zinc-300 ${context.currentNote === note.id ? 'font-bold bg-zinc-400 text-slate-100':'font-normal text-zinc-700'}`}>
             <div
             onClick={() => context.setCurrentNote(note.id) }
-            className={`px-4 py-1   w-full cursor-pointer font-mono tracking-tighter  ${context.currentNote === note.id ? 'font-bold bg-zinc-400 text-slate-100':'font-normal text-zinc-500'}`}>
+            className={`px-4 py-1 truncate overflow-hidden text-sm w-full cursor-pointer font-mono tracking-tighter `}>
                 { note.title !== "" ? note.title : "Untitled" }
             </div>
             <Dropdown class="py-auto" >
