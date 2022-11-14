623eabacb82056b8ef4e7c8f02336426bd3b2f18
Rewel Cabiles <rewel.cabiles@gmail.com>
2022-11-14 23:58:43 +0800

    Changed icon and updated product name

--- src/components/Statusbar/Statusbar.jsx
+++ src/components/Statusbar/Statusbar.jsx
@@ -10,7 +10,7 @@ export default function Statusbar() {
     return (
         <div className="w-full text-zinc-200 bg-zinc-900 lg:px-4 px-2 py-2 flex justify-between">
             <div className="lg:font-extrabold lg:w-1/3 lg:text-lg mr-auto flex flex-row cursor-pointer" onClick={ () => { context.setExtendedState(!context.extended) } }>
-                <div className="py-auto align-middle my-auto mr-1 tracking-widest uppercase hidden lg:block">Currently</div>
+                <div className="py-auto align-middle my-auto mr-1 tracking-widest uppercase hidden lg:block">Currently Notes</div>
                 <Bars3Icon className={`${context.extended ? 'rotate-180' : ''} transition-all w-7 h-7 my-auto lg:ml-2`} />
             </div>
 
