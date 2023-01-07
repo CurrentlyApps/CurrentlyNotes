22fa14346a5753d6e8d31b62f27a9e985c65595a
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-07 23:26:50 +0800

    Allow edit of title from status bar

--- src/components/Statusbar/StatusBarNoteTitle.jsx
+++ src/components/Statusbar/StatusBarNoteTitle.jsx
@@ -1,14 +1,55 @@
 import {AppContext} from "contexts/AppContext";
-import {useContext} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
+import {updateNote} from "../../services/firebase";
+import {useParams} from "react-router-dom";
+import {PencilSquareIcon} from "@heroicons/react/24/outline";
 
 export default function StatusBarNoteTitle () {
   const context = useContext(AppContext);
-  // Editable title
+  const inputRef = useRef(null);
+
+  const { user_id } = useParams();
+  const [editTitle, setEditTitle] = useState(false);
+
+  const updateTitle = function(event){
+    let tempNote = { ...context.note, title: event.target.value }
+    updateNote(tempNote, user_id)
+  }
+
+  useEffect(() => {
+    if (editTitle) {
+      inputRef.current?.focus()
+    }
+  } , [editTitle]);
+
+  if (context.note == null) {
+    return null;
+  }
+
+
+  if (!editTitle) {
+    return (
+      <div hidden={editTitle}
+        className="duration-75 transition-all hover:underline underline-offset-8 px-4  cursor-pointer text-zinc-100 my-auto flex flex-r0w"
+        onClick={ () => setEditTitle(true)} >
+        { context.note.title }
+        <PencilSquareIcon className={"w-3 my-auto ml-2 text-zinc-300"} />
+      </div>
+    );
+  }
   return (
-      <div className="my-auto p-0">
-        <div className="flex flex-row">
-          <div className="hidden lg:block">{ context.note ? context.note.title : '' }</div>
-        </div>
+      <div className="flex flex-row">
+        <input
+            type={"text"}
+            hidden={!editTitle}
+            ref={inputRef}
+            className={"appearance-none bg-zinc-900 text-sm p-0"}
+            value={ context.note.title }
+            onChange={ updateTitle }
+            onBlur={ () => { setEditTitle(false) } }
+            placeholder="Title"
+        />
+
       </div>
   )
 }
