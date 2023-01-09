94b1de9f58ed3fe5ea4ee9d6125bc2a412225ecd
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-09 22:16:01 +0800

    Cleaned up comments

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 exports.createUserDefaultData = functions.auth.user().onCreate((user) => {
   admin.database().ref(`notes/users/${user.uid}/notes`).set({
