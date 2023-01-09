8685d0cd779a912dad9ceb638b7950c205ee6658
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-09 17:00:09 +0800

    Added loading spinners to login buttons

--- src/components/Auth/SignIn/SignIn.jsx
+++ src/components/Auth/SignIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Button, Checkbox, Label, TextInput} from "flowbite-react";
+import {Button, Checkbox, Label, Spinner, TextInput} from "flowbite-react";
 import authService from "services/firebaseAuthService";
 
 export default function SignIn({setIsLoginScreen}) {
@@ -8,11 +8,33 @@ export default function SignIn({setIsLoginScreen}) {
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(null);
 
-  const signIn = () => {
-    authService.signInWithPassword(email, password, () => {}, (error) => {
-      if (error.errorCode === 'auth/wrong-password' || error.errorCode === 'auth/user-not-found') {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingGoogle, setIsLoadingGoogle] = useState(false);
+  const signInWithPasswordClick = () => {
+    setIsLoading(true);
+    authService.signInWithPassword(email, password, () => {
+      setIsLoading(false);
+    }, (error) => {
+      if (
+        error.code === 'auth/wrong-password'
+        || error.code === 'auth/user-not-found'
+        || error.code === 'auth/invalid-email'
+      ) {
         setError('Check your email and password and try again.');
+      } else {
+        setError(error.code );
       }
+      setIsLoading(false);
+    });
+  }
+
+  const signInWithGoogleClick = () => {
+    setIsLoadingGoogle(true);
+    authService.signInWithGoogle(() => {
+      setIsLoadingGoogle(false);
+    }, (error) => {
+      setError(error.code);
+      setIsLoadingGoogle(false);
     });
   }
 
@@ -75,17 +97,29 @@ export default function SignIn({setIsLoginScreen}) {
         </a>
       </div>
       <div className="w-full">
-        <Button onClick={ signIn } >
-          Log in to your account
+        <Button onClick={ signInWithPasswordClick } >
+          {
+            isLoading ?
+              <Spinner className="w-5 h-5 text-white" />
+              :
+              <span className={"pl-3"}>Log in to your account</span>
+          }
         </Button>
       </div>
       <div className={"w-full text-center"}>
         Or
       </div>
-      <Button color="light" className={"btn p-2 btn-primary mx-auto"} onClick={() => authService.signInWithGoogle()}>
+      <Button color="light" className={"btn p-2 btn-primary mx-auto"} onClick={() => signInWithGoogleClick()}>
         <div className={"flex flex-row "}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" className="w-6 h-6 mr-2" alt="Google Logo"/>
-          Sign In with Google
+          {
+            isLoadingGoogle ?
+              <Spinner className="w-5 h-5 text-white" />
+              :
+              <>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" className="w-6 h-6 mr-2" alt="Google Logo"/>
+                Sign In with Google
+              </>
+          }
         </div>
       </Button>
       <button className="w-full text-center text-sm text-blue-700 hover:underline dark:text-blue-500" onClick={() => setIsLoginScreen(false)}>
