0e73806820d6dcd18e423f15142ce699b45fab1b
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-09 14:47:00 +0800

    Hide editor if not signed in

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -8,6 +8,10 @@ export default function Editor() {
   const sidebarExtended = useSelector((state) => state.ui.sidebarExtended);
   const sidebarClass = sidebarExtended ?  'lg:w-4/5 w-1/5' : 'w-full';
 
+  const isSignedIn = useSelector((state) => state.auth.isSignedIn);
+  if (!isSignedIn) {
+    return '';
+  }
   return (
       <div className={`pt-5 flex lg:flex-row flex-col transition-all h-full overflow-hidden ${sidebarClass}`}>
           <Outlet/>
