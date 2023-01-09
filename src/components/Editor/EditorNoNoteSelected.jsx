36b69fcd66f86032e3d2d3b82d541de99370f0ab
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-09 14:45:13 +0800

    Updated no note text

--- src/components/Editor/EditorNoNoteSelected.jsx
+++ src/components/Editor/EditorNoNoteSelected.jsx
@@ -2,8 +2,8 @@
 export default function EditorNoNoteSelected() {
 
     return (
-        <div className="pt-10 w-full grow lg:py-4 text-zinc-500 text-center lg:w-4/5 "> 
-
+        <div className="mt-52 w-full grow lg:py-4 text-zinc-400 text-center text-2xl font-semibold lg:w-4/5 ">
+            Select a note on the left, or create a new note to get started.
         </div>   
     )
 }
