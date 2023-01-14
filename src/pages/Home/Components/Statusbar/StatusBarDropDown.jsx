484939abad468e07a1408c5c04042d4cc65ee2de
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-15 00:22:56 +0800

    Updated profile in statusbar

--- src/pages/Home/Components/Statusbar/StatusBarDropDown.jsx
+++ src/pages/Home/Components/Statusbar/StatusBarDropDown.jsx
@@ -1,4 +1,4 @@
-import { Dropdown } from "flowbite-react";
+import {Avatar, Dropdown} from "flowbite-react";
 import LoginOrProfile from "./LoginOrProfile";
 import {ArrowRightOnRectangleIcon, Cog6ToothIcon, HomeModernIcon, UserIcon} from "@heroicons/react/24/outline";
 import {useDispatch, useSelector} from "react-redux";
@@ -6,10 +6,22 @@ import {openModal} from "stores/UI/uiModals";
 import authService from "services/firebaseAuthService";
 export default function StatusBarDropDown() {
     const user = useSelector(state => state.auth);
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const isOnline = useSelector(state => state.firebase.isOnline);
     return (
         <div className="my-auto p-0">
-            <Dropdown label={ user.displayName } inline={true}>
+            <Dropdown label={
+                <Avatar img={user.photoURL} size={"sm"} rounded={true} status={isOnline ? '' : 'busy'}>
+                    <div className="text-sm font-medium dark:text-white">
+                        <div className={"text-left"}>
+                            { user.displayName }
+                        </div>
+                        <div className="text-xs text-gray-500 dark:text-gray-400">
+                            { user.email === user.displayName ? "" : user.email }
+                        </div>
+                    </div>
+                </Avatar>
+            } inline={true}>
                 <Dropdown.Header>
                     <LoginOrProfile/>
                 </Dropdown.Header>
