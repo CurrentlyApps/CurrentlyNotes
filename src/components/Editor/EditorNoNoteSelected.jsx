e76c5003285b057f793c7402f54184a916827e23
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-14 17:41:28 +0800

    Updated no note message on mobile

--- src/components/Editor/EditorNoNoteSelected.jsx
+++ src/components/Editor/EditorNoNoteSelected.jsx
@@ -2,7 +2,7 @@
 export default function EditorNoNoteSelected() {
 
   return (
-    <div className="mt-52 w-full grow lg:py-4 lg:mx-auto text-zinc-400 text-center text-2xl font-semibold lg:w-4/5 ">
+    <div className="mt-52 w-full grow md:py-4 px-4 md:mx-auto text-zinc-400 text-center md:text-2xl text-lg font-semibold lg:w-4/5 ">
         Select a note on the left, or create a new note to get started.
     </div>
   )
