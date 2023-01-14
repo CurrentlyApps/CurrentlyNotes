19c48efab24f9515a614181b2899590356a90d20
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-14 18:04:46 +0800

    Allow download note as .md file

--- src/components/Editor/EditorNote.jsx
+++ src/components/Editor/EditorNote.jsx
@@ -21,6 +21,8 @@ export default function EditorNote() {
 
   converter.setFlavor('github');
   const [showPreview, setShowPreview] = useState(true)
+  const note_content = useSelector(state => state.notes.note_content);
+  const note_meta = useSelector(state => state.notes.note_meta);
   const updateBody = function(event) {
     firebaseNotesService.updateNoteContent({
       ...note_content,
@@ -41,8 +43,17 @@ export default function EditorNote() {
     }
   }
 
-  const note_content = useSelector(state => state.notes.note_content);
-  const note_meta = useSelector(state => state.notes.note_meta);
+  const handleDownloadToMD = () => {
+    const element = document.createElement("a");
+    const file = new Blob([note_content.body], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = `${note_meta.title}.md`;
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+
+  }
+
+
 
   if (!note_content.id) {
     return (
@@ -63,13 +74,20 @@ export default function EditorNote() {
           <button onClick={() => setShowPreview((prevState) => {return !prevState })} className="text-zinc-700 hover:text-zinc-600">
             Toggle Preview
           </button>
-          <a
-            className={"ml-auto text-sm"}
-            target={"_blank"}
-            href={"https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax"} rel="noreferrer">
-            Markdown Guide
-          </a>
-          <hr />
+          <div className={"ml-auto"}>
+            <a
+              className={"mr-6 text-sm"}
+              target={"_blank"}
+              href={"https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax"} rel="noreferrer">
+              Markdown Guide
+            </a>
+            <button
+              className={"ml-auto text-sm"}
+              onClick={ () => handleDownloadToMD()}
+              >
+              Download as MD
+            </button>
+          </div>
         </div>
 
         <textarea
