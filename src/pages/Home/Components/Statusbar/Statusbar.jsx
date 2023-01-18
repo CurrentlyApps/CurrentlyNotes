1b8d53418e019bd2c3de04a8cdaef3dfb5ed3936
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-01-19 02:24:38 +0800

    Show icon next to title if user is editing someone elses note

--- src/pages/Home/Components/Statusbar/Statusbar.jsx
+++ src/pages/Home/Components/Statusbar/Statusbar.jsx
@@ -1,17 +1,18 @@
-import { Bars3Icon } from "@heroicons/react/24/solid"
+import {Bars3Icon, UsersIcon} from "@heroicons/react/24/outline"
 import StatusBarDropDown from "./StatusBarDropDown";
 import StatusBarNoteTitle from "./StatusBarNoteTitle";
 
 import {useDispatch, useSelector} from "react-redux";
 import {toggleSidebar} from "stores/UI/uiSlice";
 import {Route, Routes} from "react-router-dom";
+import {getAuth} from "firebase/auth";
 
 export default function Statusbar() {
   const sidebarExtended = useSelector((state) => state.ui.sidebarExtended)
-
+  const note_meta = useSelector(state => state.notes.note_meta)
   const dispatch = useDispatch()
   const user = useSelector(state => state.auth);
-
+  const current_user = getAuth().currentUser;
 
 
   return (
@@ -26,7 +27,14 @@ export default function Statusbar() {
           <div className="lg:mx-auto md:w-1/3 grow flex justify-center ">
 
             <Routes>
-              <Route path="/edit/:user_id/:note_id" element={<StatusBarNoteTitle/>} />
+              <Route path="/edit/:user_id/:note_id" element={
+                <>
+                  <StatusBarNoteTitle/>
+                  { note_meta.user_id !== current_user.uid ?
+                    <UsersIcon className={"w-4 my-auto"}/>
+                    : ""}
+                </>
+              } />
             </Routes>
 
           </div>
