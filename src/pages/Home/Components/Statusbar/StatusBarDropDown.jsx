8f88a49daa09dbfa3b8bc230ccae3592c74df207
Rewel Cabiles <rewel.cabiles@gmail.com>
2023-02-22 23:28:24 +0800

    Removed unused link

--- src/pages/Home/Components/Statusbar/StatusBarDropDown.jsx
+++ src/pages/Home/Components/Statusbar/StatusBarDropDown.jsx
@@ -1,34 +1,13 @@
 import {Avatar, Dropdown} from "flowbite-react";
 import LoginOrProfile from "./LoginOrProfile";
-import {ArrowRightOnRectangleIcon, Cog6ToothIcon, HomeModernIcon, UserIcon} from "@heroicons/react/24/outline";
+import {ArrowRightOnRectangleIcon, HomeModernIcon, UserIcon} from "@heroicons/react/24/outline";
 import {useDispatch, useSelector} from "react-redux";
 import {openModal} from "stores/UI/uiModals";
 import authService from "services/firebaseAuthService";
-import { getFunctions, httpsCallable} from "firebase/functions";
 export default function StatusBarDropDown() {
     const user = useSelector(state => state.auth);
     const dispatch = useDispatch();
     const isOnline = useSelector(state => state.firebase.isOnline);
-
-    const functions = getFunctions();
-    const shareNoteToUser = httpsCallable(functions, 'shareNoteToUser');
-
-    const handleShareNote = () => {
-        shareNoteToUser({
-            canCollab: true,
-            canPublish: true,
-            allowPublishPublic: true,
-            allowCollabPublic: false,
-            collabUsers: ['orange.grass.850@example.com'],
-            publishUsers: [],
-            noteMeta: {
-                created_at: 1673777488490,
-                id: '0',
-                title: 'test',
-                user_id: 'uDzAxJxrIem7HY8sRIo0Wedg1H4H',
-            }
-        });
-    }
 
     return (
         <div className="my-auto p-0">
@@ -58,10 +37,6 @@ export default function StatusBarDropDown() {
                             <Dropdown.Item onClick={ () => dispatch(openModal('AccountSettings'))}>
                                 <UserIcon className="w-4 mr-2"/>
                                 Account Settings
-                            </Dropdown.Item>
-                            <Dropdown.Item onClick={ ()=>handleShareNote()}>
-                                <Cog6ToothIcon className={"w-4 mr-2"} />
-                                App Settings
                             </Dropdown.Item>
                             <Dropdown.Divider />
                             <Dropdown.Item  onClick={ () => {authService.signOut()}}>
